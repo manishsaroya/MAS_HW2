solve the bug in targetCell calculation: added exploration rate: wrote environment reset function: timestep is for visualization: added Qlearning function

define_grid.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from  matplotlib import animation
 import sys
+import random
 
 def moveLeft(zvals_, pose):
     newPose = pose
@@ -22,8 +23,8 @@ def moveRight(zvals_,pose):
         zvals_[pose[0], pose[1]] = 0
         newPose[1] = pose[1] + 1
         #zvals_[newPose[0], newPose[1]] = t
-    else:
-        print "wall ahead"
+    #else:
+        #print "wall ahead"
     #im.set_data(zvals_)
     return zvals_, newPose
 
@@ -35,8 +36,8 @@ def moveUp(zvals_,pose):
         zvals_[pose[0], pose[1]] = 0
         newPose[0] = pose[0] + 1
         #zvals_[newPose[0], newPose[1]] = t
-    else:
-        print "wall ahead"
+    #else:
+        #print "wall ahead"
     #im.set_data(zvals_)
     return zvals_, newPose
 
@@ -47,15 +48,15 @@ def moveDown(zvals_,pose):
         zvals_[pose[0], pose[1]] = 0
         newPose[0] = pose[0] - 1
         #zvals_[newPose[0], newPose[1]] = t
-    else:
-        print "wall ahead"
+    #else:
+        #print "wall ahead"
     #im.set_data(zvals_)
     return zvals_, newPose
 
 
 # Some global learning parameters
-numIterations = 100 # these many times the robot will catch the target while in training mode.
-iterations = 0
+numIterations = 10000 # these many times the robot will catch the target while in training mode.
+#iterations = 0
 Qvalues = np.zeros((2500,4)) #Qvalue is [States * Actions] matrix.
 # the states are 50(target positions) * 50 (robot positions) 
 # at every state the robot can take 4 actions.
@@ -69,6 +70,9 @@ TerminateReward = 20
 # learning rate between 0 and 1.
 learningRate = 0.5
 
+# exploration rate
+epsilon = 0.3
+
 # the map 
 zvals = np.zeros((5,10))
 
@@ -76,9 +80,26 @@ zvals = np.zeros((5,10))
 robotPose = [np.random.randint(0,5), np.random.randint(0,10)]
 targetPose = [1,9]
 
+def envReset():
+        # the map
+        global zvals
+        zvals = np.zeros((5,10))
+
+        # intialize the random robot pose and fixed target pose.
+        global robotPose
+        robotPose = [np.random.randint(0,5), np.random.randint(0,10)]
+        global targetPose
+        targetPose = [1,9]
+
+        # assign color/ID values to robotpose and target pose in map
+        zvals[robotPose[0],robotPose[1]] = 1
+        zvals[targetPose[0],targetPose[1]] = 0.5
+
+envReset()
+
 # assign color/ID values to robotpose and target pose in map
-zvals[robotPose[0],robotPose[1]] = 1
-zvals[targetPose[0],targetPose[1]] = 0.5
+#zvals[robotPose[0],robotPose[1]] = 1
+#zvals[targetPose[0],targetPose[1]] = 0.5
 
 # Visualization
 fig = plt.figure()
@@ -87,14 +108,12 @@ im = plt.imshow(zvals, origin={'lower','left'})
 def init():
     im.set_data(zvals)
 
-def timestep(i):
-    
+def timestep(i):  
     # Before action mapping from robotPose & targetPose to Qvalues table.
     robotCell = len(zvals[0]) * robotPose[0] + robotPose[1]
-    targetCell = len(zvals[0]) * targetPose[0] + robotPose[1]
+    targetCell = len(zvals[0]) * targetPose[0] + targetPose[1]
     state_ = (len(zvals[0]) * len(zvals)) * robotCell + targetCell
-
-
+    
     #move the target randomly.
     switcher = {0: moveLeft, 1: moveRight, 2: moveUp, 3: moveDown}
     func = switcher.get(np.random.randint(0,4), lambda: "Invalid input")
@@ -102,60 +121,75 @@ def timestep(i):
     
     #move the robot 
     switcher = {0: moveLeft, 1: moveRight, 2: moveUp, 3: moveDown}
-    action = np.random.randint(0,4)
+    action = np.argmax(Qvalues[state_])
     func = switcher.get(action, lambda: "Invalid input")
     newzvals, newrobotPose = func(zvals, robotPose)
-    
 
+    # assign color/ID values to robotpose and target pose in map
+    zvals[robotPose[0],robotPose[1]] = 1
+    zvals[targetPose[0],targetPose[1]] = 0.5
+    
     # After action mapping from robotPose & targetPose to Qvalues table. 
     robotCell = len(zvals[0]) * robotPose[0] + robotPose[1]
-    targetCell = len(zvals[0]) * targetPose[0] + robotPose[1]
-    state = ((len(zvals[0]) * len(zvals)) * robotCell)+ targetCell 
-    print robotCell , targetCell, state, (len(zvals[0]) * len(zvals[1]))
-
-    #Change the Zvals;
-    if targetCell!=robotCell:
-        #update the Qtable
-        sample = transitionReward + np.amax(Qvalues[state])
-        Qvalues[state_,action] = ((1 - learningRate) * Qvalues[state_,action]) + (learningRate * sample)
-    else:
-        #update the Qtable
-        sample = transitionReward + 20
-        Qvalues[state_,action] = ((1 - learningRate) * Qvalues[state_,action]) + (learningRate * sample)
-
-        global iterations
-        iterations = iterations + 1
-        # the map
-        global zvals
-        zvals = np.zeros((5,10))
-
-        # intialize the random robot pose and fixed target pose.
-        global robotPose
-        robotPose = [np.random.randint(0,5), np.random.randint(0,10)]
-        global targetPose 
-        targetPose = [1,9]
-
-        # assign color/ID values to robotpose and target pose in map
-        zvals[robotPose[0],robotPose[1]] = 1
-        zvals[targetPose[0],targetPose[1]] = 0.5
-
-        if iterations == numIterations:
-            for i in range(len(Qvalues)):
-                if i%50 == 4:
-                    print Qvalues[i]
-            sys.exit("Target Found")
-    #if cmp(targetPose, robotPose):
-    #    zvals[robotPose[0],robotPose[1]] = 1
-    #    zvals[targetPose[0],targetPose[1]] = 0.5
-    #else:
-    #    sys.exit("TARGET FOUND")
-    
-    # visualize
-    print zvals
+    targetCell = len(zvals[0]) * targetPose[0] + targetPose[1]
+   
+    print robotPose, targetPose, robotCell, targetCell
     im.set_data(zvals)
+    if robotCell == targetCell:
+        sys.exit("Demo done")
+
+def Qlearning():
+    iterations = 0
+    while iterations < numIterations:
+        # Before action mapping from robotPose & targetPose to Qvalues table.
+        robotCell = len(zvals[0]) * robotPose[0] + robotPose[1]
+        targetCell = len(zvals[0]) * targetPose[0] + targetPose[1]
+        state_ = (len(zvals[0]) * len(zvals)) * robotCell + targetCell
     
+    
+        #move the target randomly.
+        switcher = {0: moveLeft, 1: moveRight, 2: moveUp, 3: moveDown}
+        func = switcher.get(np.random.randint(0,4), lambda: "Invalid input")
+        newzvalstarget, newtargetPose = func(zvals,targetPose)
+        
+        #move the robot 
+        switcher = {0: moveLeft, 1: moveRight, 2: moveUp, 3: moveDown}
+        if random.uniform(0,1) < epsilon:
+            action = np.random.randint(0,4)
+        else:
+            action = np.argmax(Qvalues[state_])
+        func = switcher.get(action, lambda: "Invalid input")
+        newzvals, newrobotPose = func(zvals, robotPose)
+        
+    
+        # After action mapping from robotPose & targetPose to Qvalues table. 
+        robotCell = len(zvals[0]) * robotPose[0] + robotPose[1]
+        targetCell = len(zvals[0]) * targetPose[0] + targetPose[1]
+        state = ((len(zvals[0]) * len(zvals)) * robotCell)+ targetCell 
+        #print robotCell , targetCell, state, (len(zvals[0]) * len(zvals[1]))
+    
+        #Change the Zvals;
+        if targetCell!=robotCell:
+            #update the Qtable
+            sample = transitionReward + np.amax(Qvalues[state])
+            Qvalues[state_,action] = ((1 - learningRate) * Qvalues[state_,action]) + (learningRate * sample)
+        else:
+            #update the Qtable
+            sample = transitionReward + 20
+            Qvalues[state_,action] = ((1 - learningRate) * Qvalues[state_,action]) + (learningRate * sample)
+    
+            #global iterations
+            iterations = iterations + 1
+            
+            envReset()
+    
+            if iterations == numIterations:
+                for i in range(len(Qvalues)):
+                    if i%50 == 4:
+                        print Qvalues[i]
+
+Qlearning()
 anim = animation.FuncAnimation(fig, timestep, init_func=init, frames=30,
-                               interval=1)
+                               interval=1000)
 
 plt.show()
-